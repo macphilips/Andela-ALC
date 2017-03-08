0a308c2129a919aee63198710f0d2e898026380b
macphilips <tmorolari@gmail.com>
2017-03-08 16:06:25 +0100

    second commit

--- app/src/main/java/com/rmhub/andela_alc/UserProfileFragment.java
+++ app/src/main/java/com/rmhub/andela_alc/UserProfileFragment.java
@@ -1,5 +1,6 @@
 package com.rmhub.andela_alc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ShareActionProvider;
 
 /**
  * Created by MOROLANI on 3/8/2017
@@ -20,6 +22,7 @@ import android.view.ViewGroup;
 public class UserProfileFragment extends Fragment {
     private static final String LOAD_USER = "user profile";
     private User user;
+    private ShareActionProvider mShareActionProvider;
 
     public static UserProfileFragment newInstance(User user) {
         Bundle args = new Bundle();
@@ -32,14 +35,20 @@ public class UserProfileFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        user =  getArguments().getParcelable(LOAD_USER);
+        user = getArguments().getParcelable(LOAD_USER);
         setHasOptionsMenu(true);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_share) {
-           return true;
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            String msg = String.format("Check out this awesome developer @%s, %s.", user.getUsername(), user.getHtmlURL());
+            sendIntent.putExtra(Intent.EXTRA_TEXT, msg);
+            sendIntent.setType("text/plain");
+            setShareIntent(sendIntent);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -47,9 +56,16 @@ public class UserProfileFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_user_profile, menu);
-        menu.findItem(R.id.action_share);
-
+        MenuItem item = menu.findItem(R.id.action_share);
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
     }
+
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
